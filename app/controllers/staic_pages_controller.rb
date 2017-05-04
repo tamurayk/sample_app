220d2195f06e3e1e15e8b4ff5d195617c28a9b57
Yukio Tamura <y.tamura.biz@gmail.com>
2017-05-04 05:10:53 +0000

    destory StaicPages for fix typo

--- app/controllers/staic_pages_controller.rb
+++
@@ -1,7 +0,0 @@
-class StaicPagesController < ApplicationController
-  def home
-  end
-
-  def help
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'staic_pages/home'
-
-  get 'staic_pages/help'
-
   root 'application#hello'
 end
